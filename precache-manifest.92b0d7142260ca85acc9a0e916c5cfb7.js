5292aca850a1fd81daf7e054f465a384702465f4
PaoloDiBello <dibellopol@gmail.com>
2019-03-22 00:56:57 +0100

    Updates

---
+++ precache-manifest.92b0d7142260ca85acc9a0e916c5cfb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "a9b80a0b0e722e0d3664",
+    "url": "/contactmanager/static/js/runtime~main.884ecd02.js"
+  },
+  {
+    "revision": "93245ad5d6fdaf7ac09d",
+    "url": "/contactmanager/static/js/main.e257044d.chunk.js"
+  },
+  {
+    "revision": "f099cc96f9dd850295b4",
+    "url": "/contactmanager/static/js/2.892ed04c.chunk.js"
+  },
+  {
+    "revision": "93245ad5d6fdaf7ac09d",
+    "url": "/contactmanager/static/css/main.07318036.chunk.css"
+  },
+  {
+    "revision": "f099cc96f9dd850295b4",
+    "url": "/contactmanager/static/css/2.a4bbce7c.chunk.css"
+  },
+  {
+    "revision": "9ad92caf26b2081c1836655ccc9f1051",
+    "url": "/contactmanager/index.html"
+  }
+];
